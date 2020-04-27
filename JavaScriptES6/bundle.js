f3d44874c25c69382f0496f2d82d22c22aac3a3f
Lucas Del Puerto <lucasgarciadragon@hotmail.com>
2020-04-27 09:36:22 -0300

    Array operations

--- JavaScriptES6/bundle.js
+++ JavaScriptES6/bundle.js
@@ -1,3 +1,20 @@
 "use strict";
 
-alert("OK");
+var arr = [1, 3, 4, 5, 8, 9];
+console.log(arr);
+var newArr = arr.map(function (item, index) {
+  return item + index;
+});
+console.log(newArr);
+var sum = arr.reduce(function (total, next) {
+  return total + next;
+});
+console.log(sum);
+var filter = arr.filter(function (item) {
+  return item % 2 === 0;
+});
+console.log(filter);
+var find = arr.find(function (item) {
+  return item === 4;
+});
+console.log(find);
--- JavaScriptES6/main.js
+++ JavaScriptES6/main.js
@@ -1,1 +1,27 @@
-alert("OK")
+const arr = [1, 3, 4, 5, 8, 9];
+
+console.log(arr);
+
+const newArr = arr.map(function(item, index){ 
+    return item + index;
+});
+
+console.log(newArr);
+
+const sum = arr.reduce(function(total, next) {
+    return total + next;
+});
+
+console.log(sum);
+
+const filter = arr.filter(function(item) {
+    return item % 2 === 0;
+});
+
+console.log(filter);
+
+const find = arr.find((item) => {
+    return item === 4;
+});
+
+console.log(find);
