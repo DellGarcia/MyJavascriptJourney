d300e21702f060122636872378a8e78507bfa742
Lucas Del Puerto <lucasgarciadragon@hotmail.com>
2020-04-28 09:37:59 -0300

    Training class creation

---
+++ JavaScriptES6/admin-user/bundle.js
@@ -0,0 +1,25 @@
+class User {
+  constructor(email, password) {
+    this.email = email;
+    this.password = password;
+    this.admin = false;
+  }
+
+  isAdmin() {
+    return this.admin;
+  }
+
+}
+
+class Admin extends User {
+  constructor(email, password) {
+    super(email, password);
+    this.admin = true;
+  }
+
+}
+
+const user = new User("email@teste.com", "1234");
+const admin = new Admin("email@teste.com", "1234");
+console.log(user.isAdmin());
+console.log(admin.isAdmin());
