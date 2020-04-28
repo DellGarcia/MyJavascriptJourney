6a48cce6f1191c88ae81df87856434b5786cd11a
Lucas Del Puerto <lucasgarciadragon@hotmail.com>
2020-04-28 17:26:15 -0300

    Train template literals and short sintax

---
+++ JavaScriptES6/template-literals/exercise/main.js
@@ -0,0 +1,12 @@
+const nome = 'Lucas';
+const idade = 18;
+
+console.log(`O usuário ${nome} possui ${idade} anos`);
+
+const usuario = {
+    nome,
+    idade,
+    cidade: 'Rio do Sul',
+};
+
+console.log(usuario);
