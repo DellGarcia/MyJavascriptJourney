57a1c1703fa47d6e796146d5ca592e54979dcf22
Lucas Del Puerto <lucasgarciadragon@hotmail.com>
2020-05-08 10:15:04 -0300

    Learn to create and add database models

--- NodeJS/api-node/server.js
+++ NodeJS/api-node/server.js
@@ -1,5 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose'); // mongoose é um ORM (Object relational mapping)
+const requireDir = require('require-dir');
 
+// Iniciando o App
 const app = express();
+
+// Iniciando o DB
+mongoose.connect(
+    "mongodb://localhost:27017/nodeapi"
+    , { useNewUrlParser: true, useUnifiedTopology: true}
+)
+
+requireDir("./src/models");
+
+const Product = mongoose.model('Product')
+
+// Rotas
+app.get('/', (req, res) => {
+    Product.create({
+        title: "Java",
+        description: "Uma grande linguagem de programação",
+        url: "http://java.com"
+    })
+
+    return res.send('Hello World');
+})
 
 app.listen(3333);
